f0aabc64749ba9554fc334c794194d349dd6ce68
Akshay Ladne <Kumawata283@gmail.com>
2025-02-24 16:19:17 +0530

    Initial commit

---
+++ 06_like_feature/index.js
@@ -0,0 +1,16 @@
+const container = document.querySelector("#container");
+const like = document.querySelector('i');
+
+container.addEventListener('dblclick', ()=>{
+    like.style.transform = "translate(-50%, -50%), scale(1)";
+    like.style.opacity = 0.8;
+    like.style.color = "rgb(233, 89, 80)";
+
+    setTimeout(()=>{
+        like.style.opacity = 0;
+    }, 1500);
+
+    setTimeout(()=>{
+        like.style.transform = "translate(-50%, -50%), scale(1)";
+    }, 1000);
+})
